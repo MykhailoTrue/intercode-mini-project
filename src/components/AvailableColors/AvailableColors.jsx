9ffcdb9e562048adc64d079a571ca21365a4d15f
Mykhailo Trufyn <mishatrufyn@gmail.com>
2024-01-24 22:50:13 +0200

    available colors

--- src/components/AvailableColors/AvailableColors.jsx
+++ src/components/AvailableColors/AvailableColors.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useFetching } from "../../hooks/useFetching";
 import { getAllColors } from "../../services/vehicleService";
 import classes from "./AvailableColors.module.scss";
+import Loader from "../UI/Loader/Loader";
 
 const AvailableColors = ({ setVehicle, vehicle }) => {
   const [availableColors, setAvailableColors] = useState([]);
@@ -16,17 +17,42 @@ const AvailableColors = ({ setVehicle, vehicle }) => {
     fetchAvailabeColors();
   }, []);
 
-  return (
-    <div className={classes.vehicleOptionsColor}>
-      {availableColors.map((color) => (
-        <div
-          key={color.id}
-          style={{ backgroundColor: color.value }}
-          onClick={(e) => setVehicle({ ...vehicle, color })}
-        >
-          {color.id} color
+  return isAvailableColorsLoading ? (
+    <Loader></Loader>
+  ) : (
+    <div className={classes.availableColors}>
+      <div className={classes.availableColorsContainer}>
+        <h4 className={classes.colorName}>{vehicle?.color?.name}</h4>
+        <div className={classes.colorsContainer}>
+          {availableColors.map((color) => (
+            <div className={classes.colorCard} key={color.id}>
+              <div
+                className={
+                  classes.colorCardContainer +
+                  (color?.id === vehicle?.color?.id
+                    ? " " + classes._active
+                    : "")
+                }
+                style={{ backgroundColor: color.value }}
+                onClick={(e) => setVehicle({ ...vehicle, color })}
+              >
+                <div className={classes.colorSelectedIconContainer}>
+                  <svg
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 114.76 100"
+                  >
+                    <polygon
+                      points="114.8,13.3 101.5,0 38.1,71.3 12.5,39.9 0,53.9 37.6,100"
+                      aria-hidden="true"
+                    ></polygon>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
